98c534681e03f087289daf6d8d5a5604f85b929e
techsavvyash <mittal.yash12@hotmail.com>
2022-07-12 22:03:32 +0530

    a web3 project
    Signed-off-by: techsavvyash <mittal.yash12@hotmail.com>

---
+++ Web3/jpegdegens/contracts/HelloWorld.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+
+// a contract looks a lot like a class
+contract HelloWorld {
+
+    function hello() public pure returns (string memory) {
+        return "Hello, World";
+    }
+
+}
