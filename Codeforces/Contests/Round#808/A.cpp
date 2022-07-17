4a2602b5652bb6838fdf4e7bfb9ffc16429fb002
techsavvyash <mittal.yash12@hotmail.com>
2022-07-17 21:34:40 +0530

    added more ques
    Signed-off-by: techsavvyash <mittal.yash12@hotmail.com>

---
+++ Codeforces/Contests/Round#808/A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> arr(n);
+
+    for (ll i = 0; i < n; i++)
+        cin >> arr[i];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
